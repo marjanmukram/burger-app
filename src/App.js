070849acdbcb035794ca7f059f594692d4ad08fe
Marjan <marjan.emeraldit@gmail.com>
2019-09-21 20:47:24 +0530

    Basic layout added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <div />
-  );
+    <div >
+      <Layout />
+    </div>);
 }
 
 export default App;
